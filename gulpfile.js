7d71ac1ea35a5cd881f467369ebe736cd05e0586
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2015-11-16 12:20:39 +0100

    generating css and js file to include in admin layout

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var elixir = require('laravel-elixir');
-
+var bowerDir = 'bower_components/';
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
@@ -12,5 +12,24 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.copy(bowerDir + 'ckeditor', 'public/ckeditor');
+    mix.copy(bowerDir + 'bootstrap/fonts', 'public/fonts');
+    mix.copy(bowerDir + 'bootstrap/dist/js/bootstrap.min.js', 'resources/assets/js/vendor/bootstrap.min.js');
+    mix.copy(bowerDir + 'jquery/dist/jquery.min.js', 'resources/assets/js/vendor/jquery.js');
+    mix.copy(bowerDir + 'ckeditor/ckeditor.js', 'resources/assets/js/vendor/ckeditor.js');
+
+    mix.copy(bowerDir + 'bootstrap/dist/css/bootstrap.min.css', 'resources/assets/css/vendor/bootstrap.min.css');
+
+    mix.styles([
+        "vendor/*.css",
+        'app.css'
+    ], 'public/css');
+
+    mix.scripts([
+            "vendor/jquery.js",
+            "vendor/ckeditor.js",
+            "vendor/bootstrap.min.js"
+        ], 'public/js', 'resources/assets/js');
+
+    mix.version(["css/all.css", "js/all.js"]);
 });
