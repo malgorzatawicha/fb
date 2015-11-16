a061a9acbe17d09250f5abc806d2aa150dea21a5
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2015-11-16 12:23:33 +0100

    change destination folder for bootstrap fonts to correct one.

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ var bowerDir = 'bower_components/';
 
 elixir(function(mix) {
     mix.copy(bowerDir + 'ckeditor', 'public/ckeditor');
-    mix.copy(bowerDir + 'bootstrap/fonts', 'public/fonts');
+    mix.copy(bowerDir + 'bootstrap/fonts', 'public/build/fonts');
     mix.copy(bowerDir + 'bootstrap/dist/js/bootstrap.min.js', 'resources/assets/js/vendor/bootstrap.min.js');
     mix.copy(bowerDir + 'jquery/dist/jquery.min.js', 'resources/assets/js/vendor/jquery.js');
     mix.copy(bowerDir + 'ckeditor/ckeditor.js', 'resources/assets/js/vendor/ckeditor.js');
