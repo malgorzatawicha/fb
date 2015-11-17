c2240a4fbc7e6c6ca4d47c406f398be764cd5e4e
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2015-11-17 10:45:10 +0100

    make slug editable.

--- app/Models/Cms/Page.php
+++ app/Models/Cms/Page.php
@@ -13,6 +13,7 @@ class Page extends Model implements SluggableInterface
     protected $sluggable = [
         'build_from' => 'title',
         'save_to'    => 'slug',
+        'on_update'  => true
     ];
 
 }
