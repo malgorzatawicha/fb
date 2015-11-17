e1ea20a5388d93046979e19de30fef650c0a06b6
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2015-11-17 10:40:58 +0100

    Make slug column unique

--- database/migrations/2015_11_11_130152_create_pages_table.php
+++ database/migrations/2015_11_11_130152_create_pages_table.php
@@ -16,7 +16,7 @@ class CreatePagesTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('title');
-            $table->string('slug');
+            $table->string('slug')->unique();
             $table->text('body');
             $table->timestamps();
         });
