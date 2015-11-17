084d2c1f04e65430935487104d4cc485f035e5cd
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2015-11-17 10:41:44 +0100

    make page model sluggable

--- app/Models/Cms/Page.php
+++ app/Models/Cms/Page.php
@@ -2,9 +2,17 @@
 
 namespace Fb\Models\Cms;
 
+use Cviebrock\EloquentSluggable\SluggableInterface;
+use Cviebrock\EloquentSluggable\SluggableTrait;
 use Illuminate\Database\Eloquent\Model;
 
-class Page extends Model
+class Page extends Model implements SluggableInterface
 {
-    //
+    use SluggableTrait;
+
+    protected $sluggable = [
+        'build_from' => 'title',
+        'save_to'    => 'slug',
+    ];
+
 }
