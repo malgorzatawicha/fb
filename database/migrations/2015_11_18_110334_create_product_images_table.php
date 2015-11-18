bc812f8c8a1b1604cbdf640c4a4820ad59d5e2fa
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2015-11-18 12:05:30 +0100

    new migration for product images table

---
+++ database/migrations/2015_11_18_110334_create_product_images_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_images', function (Blueprint $table) {
+            $table->increments('id');
+            $table->boolean('active')->default(false);
+            $table->boolean('is_featured')->default(false);
+            $table->string('image_name')->unique();
+            $table->string('image_path');
+            $table->string('image_extension', 10);
+            $table->string('mobile_image_name')->unique();
+            $table->string('mobile_image_path');
+            $table->string('mobile_extension', 10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('product_images');
+    }
+}
