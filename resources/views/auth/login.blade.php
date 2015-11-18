94498c863ab98696f45762b48811dcda3b4d3e75
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2015-11-18 10:34:31 +0100

    change styles of login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,61 @@
 @extends('layouts/admin_login')
 @section('content')
-
-    <div class="row center-box" id="panel-login">
-        <div class="col-lg-4 col-lg-offset-4">
-            <form method="POST" action="{{ route('auth.login') }}" accept-charset="UTF-8">
-
-                <input name="_token" type="hidden" value="{{ csrf_token() }}">
-
-                <div class="panel panel-default">
-
-                    <div class="panel-heading">
-                        <h4 class="text-center text-uppercase">Login</h4>
-                    </div>
-
-                    <div class="panel-body">
-                        @include('common.errors')
-
-
-                        <div class="form-group {{ $errors->has('email') ? 'has-error has-feedback' : null }}">
-                            <label class="control-label" for="email">Email</label>
-                            <input type="email" name="email" id="email" class="form-control"/>
-                            @if ($errors->has('email'))
-                                @foreach($errors->get('email') as $message)
-                                    <p class="help-block">{{ $message }}</p>
-                                @endforeach
-                            @endif
-                        </div>
-                        <div class="form-group {{ $errors->has('password') ? 'has-error has-feedback' : null }}">
-                            <label class="control-label" for="password">Password</label>
-                            <input class="form-control" type="password" name="password" id="password"/>
-                            @if ($errors->has('password'))
-                                @foreach($errors->get('password') as $message)
-                                    <p class="help-block">{{ $message }}</p>
-                                @endforeach
-                            @endif
-                        </div>
-                        <div class="checkbox">
-                            <label>
-                                <input type="checkbox" name="remember-me" value="0"/> Remember me
-                            </label>
-                        </div>
-
-                    </div>
-
-                    <div class="panel-footer text-center">
-                        <button type="submit" class="btn btn-primary">Login</button>
-                    </div>
-
-                </div>
-
-            </form>
+    <form class="form-signin" method="POST" action="{{ route('auth.login') }}" accept-charset="UTF-8">
+        @include('common/errors')
+        <h2 class="form-signin-heading">{{trans('Login')}}</h2>
+        <input name="_token" type="hidden" value="{{ csrf_token() }}">
+        <label class="sr-only" for="email">Email</label>
+        <input type="email" name="email" id="email" class="form-control"/>
+        <label class="sr-only" for="password">Password</label>
+        <input class="form-control" type="password" name="password" id="password"/>
+        <div class="checkbox">
+            <label>
+                <input type="checkbox" name="remember-me" value="0"/> Remember me
+            </label>
         </div>
-    </div>
+        <button type="submit" class="btn btn-primary btn-block">Login</button>
+    </form>
+@stop
 
+@section('styles')
+    <style>
+
+        body {
+            background-color: #eee;
+            padding-bottom: 40px;
+            padding-top: 40px;
+        }
+        .form-signin {
+            margin: 0 auto;
+            max-width: 330px;
+            padding: 15px;
+        }
+        .form-signin .form-signin-heading, .form-signin .checkbox {
+            margin-bottom: 10px;
+        }
+        .form-signin .checkbox {
+            font-weight: normal;
+        }
+        .form-signin .form-control {
+            box-sizing: border-box;
+            font-size: 16px;
+            height: auto;
+            padding: 10px;
+            position: relative;
+        }
+        .form-signin .form-control:focus {
+            z-index: 2;
+        }
+        .form-signin input[type="email"] {
+            border-bottom-left-radius: 0;
+            border-bottom-right-radius: 0;
+            margin-bottom: -1px;
+        }
+        .form-signin input[type="password"] {
+            border-top-left-radius: 0;
+            border-top-right-radius: 0;
+            margin-bottom: 10px;
+        }
+
+    </style>
 @stop
