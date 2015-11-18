c415031c3b1592879e160fd1398d355d46896885
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2015-11-18 12:08:17 +0100

    new model product image

---
+++ app/Models/Shop/ProductImage.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Fb\Models\Shop;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductImage extends Model
+{
+    protected $table = 'product_images';
+
+    protected $fillable = [
+        'active',
+        'is_featured',
+        'image_name',
+        'image_path',
+        'image_extension',
+        'mobile_image_name',
+        'mobile_image_path',
+        'mobile_extension'
+    ];
+}
