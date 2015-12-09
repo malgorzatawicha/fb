4cf844aa62a96912b73d7c61e588b7dc92a21d5f
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2015-12-09 21:15:28 +0100

    displaying product images in grid, and displaying its action buttons

--- resources/views/shop/products/partial/images.blade.php
+++ resources/views/shop/products/partial/images.blade.php
@@ -1,9 +1,20 @@
 @if (count($product->images)>0)
-    <div class="row">
+    <div class="row product-images">
         @foreach($product->images as $image )
-            <div class="col-sm-4"> <a href="{{route('admin.products.images.show', ['product' => $product->slug, 'image' =>$image->id])}}">
-                    <img src="/images/products/thumbnails/{{ 'thumb-'. $image->image_name . '.' .
-           $image->image_extension . '?'. 'time='. time() }}"> </a></div>
+            <div class="col-sm-3 thumb">
+                    <a class="thumbnail" href="{{route('admin.products.images.show', ['product' => $product->slug, 'image' =>$image->id])}}">
+                        <img class="img-responsive" src="/images/products/thumbnails/{{ 'thumb-'. $image->image_name . '.' .
+           $image->image_extension . '?'. 'time='. time() }}">
+                    </a>
+                <div class="btn-group btn-group-justified">
+                    <a class="btn btn-primary btn-sm" title="edit" data-imageid="{{$image->id}}" href="#">
+                        <i class="glyphicon glyphicon-edit"></i>
+                    </a>
+                    <a class="btn btn-danger btn-sm" title="delete" data-imageid="{{$image->id}}" href="#">
+                        <i class="glyphicon glyphicon-trash"></i>
+                    </a>
+                </div>
+            </div>
         @endforeach
     </div>
 @else
