59955f187cb559191dac2cd00f863de3318408ca
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2015-12-12 10:33:35 +0100

    changes in gulp file and composer json for new vendor package

--- gulpfile.js
+++ gulpfile.js
@@ -13,14 +13,18 @@ var elixir = require('laravel-elixir');
 elixir(function(mix) {
     mix
         .copy('vendor/twbs/bootstrap/dist/fonts', 'public/build/fonts/')
-
+        .copy('resources/assets/js/admin.products.js', 'public/js/admin.products.js')
         .scripts([
             "../../../vendor/components/jquery/jquery.min.js",
-            "../../../vendor/twbs/bootstrap/dist/js/bootstrap.min.js"
+            '../../../vendor/kartik-v/bootstrap-fileinput/js/plugins/canvas-to-blob.min.js',
+            '../../../vendor/kartik-v/bootstrap-fileinput/js/fileinput.min.js',
+            "../../../vendor/twbs/bootstrap/dist/js/bootstrap.min.js",
+            "../../../vendor/kartik-v/bootstrap-fileinput/js/fileinput_locale_pl.js"
         ], 'public/js')
 
         .styles([
             '../../../vendor/twbs/bootstrap/dist/css/bootstrap.min.css',
+            '../../../vendor/kartik-v/bootstrap-fileinput/css/fileinput.min.css',
             'app.css'
         ], 'public/css')
 
