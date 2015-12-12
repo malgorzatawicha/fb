aff9152f4ca95410c357417c42c798a9a40226cd
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2015-12-12 10:59:19 +0100

    js that prepares submit button in edit and create image modal.

--- resources/assets/js/admin.products.js
+++ resources/assets/js/admin.products.js
@@ -5,6 +5,17 @@ function getDataForModal(event, data) {
     return button.data(data);
 }
 
+function prepareSubmitButton(event, modal)
+{
+    var submitAction = getDataForModal(event, 'submit-action');
+    var $form = $('form', modal);
+    $form.attr('action', submitAction);
+    $(".submit-form", modal).click(function() {
+        $form.submit();
+    })
+}
+
+
 $('#createImageModal').on('show.bs.modal', function(event) {
     var product = getDataForModal(event, 'product');
     var modal = $(this);
@@ -14,10 +25,12 @@ $('#createImageModal').on('show.bs.modal', function(event) {
     $("#mobile-image", modal).fileinput({
         showUpload: false
     });
+    prepareSubmitButton(event, modal);
 });
 $('#editImageModal').on('show.bs.modal', function (event) {
     var image = getDataForModal(event, 'image');
     var product = getDataForModal(event, 'product');
+    var submitAction = getDataForModal(event, 'submit-action');
 
     var modal = $(this);
     modal.find('.modal-title').text('Edit image ' + image.image_name);
@@ -34,4 +47,6 @@ $('#editImageModal').on('show.bs.modal', function (event) {
         initialPreview: '<img src="' + image.image_path +'/thumbnails/thumb-' + image.image_name + '.' + image.image_extension + '">',
         showUpload: false
     });
-})
+
+    prepareSubmitButton(event, modal);
+});
