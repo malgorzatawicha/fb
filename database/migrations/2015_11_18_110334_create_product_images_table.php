2ae25c53ab377a85ae197830dcab7d2354b24539
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2015-12-12 13:45:24 +0100

    changes in columns of product images table

--- database/migrations/2015_11_18_110334_create_product_images_table.php
+++ database/migrations/2015_11_18_110334_create_product_images_table.php
@@ -19,12 +19,23 @@ class CreateProductImagesTable extends Migration
             $table->foreign('product_id')->references('id')->on('products');
             $table->boolean('active')->default(false);
             $table->boolean('is_featured')->default(false);
+
             $table->string('image_name')->unique();
             $table->string('image_path');
             $table->string('image_extension', 10);
-            $table->string('mobile_image_name')->unique();
-            $table->string('mobile_image_path');
+
+            $table->string('image_thumbnail_name')->unique();
+            $table->string('image_thumbnail_path');
+            $table->string('image_thumbnail_extension', 10);
+
+            $table->string('mobile_name')->unique();
+            $table->string('mobile_path');
             $table->string('mobile_extension', 10);
+
+            $table->string('mobile_thumbnail_name')->unique();
+            $table->string('mobile_thumbnail_path');
+            $table->string('mobile_thumbnail_extension', 10);
+
             $table->timestamps();
         });
     }
