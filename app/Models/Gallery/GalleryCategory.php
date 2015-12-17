00b9bf7550bcae8009ca95412ab1fee90d952f00
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2015-12-17 18:51:35 +0100

    new model gallery category

---
+++ app/Models/Gallery/GalleryCategory.php
@@ -0,0 +1,17 @@
+<?php namespace Fb\Models\Gallery;
+
+use Baum\Node;
+
+/**
+* GalleryCategory
+*/
+class GalleryCategory extends Node {
+
+  /**
+   * Table name.
+   *
+   * @var string
+   */
+  protected $table = 'gallery_categories';
+
+}
