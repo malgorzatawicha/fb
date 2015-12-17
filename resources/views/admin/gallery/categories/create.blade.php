a0d4fe3a8493dd22fb5e4edeb0ef23b5095653d5
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2015-12-17 19:09:39 +0100

    new view for category creation

---
+++ resources/views/admin/gallery/categories/create.blade.php
@@ -0,0 +1,29 @@
+@extends('admin.layouts.default')
+
+@section('content')
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h4>{{trans('gallery.categories')}} - {{trans('admin.create')}}</h4>
+        </div>
+        <div class="panel-body">
+            @include('common.errors')
+            <form action="{{ route('admin.gallery.categories.store') }}" method="POST" class="form-horizontal">
+                {{ csrf_field() }}
+                <div class="form-group">
+                    <label for="category-name" class="col-sm-3 control-label">{{ trans('gallery.category.name') }}</label>
+                    <div class="col-sm-6">
+                        <input type="text" name="name" id="category-name" class="form-control"/>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-offset-3 col-sm-6">
+                        <a href="{{route('admin.gallery.categories.index')}}" class="btn btn-primary">{{ trans('admin.back') }}</a>
+                        <button type="submit" class="btn btn-default">
+                            <span class="glyphicon glyphicon-plus-sign"></span> {{ trans('gallery.category.add') }}
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+@stop
