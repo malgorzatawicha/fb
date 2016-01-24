0e49c44badd91675fb15fe4fa84b2da69f1a207e
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2016-01-24 11:14:13 +0100

    new request for site editing

---
+++ app/Http/Requests/Shop/Site/EditSiteRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Fb\Http\Requests\Site;
+
+use Fb\Http\Requests\Request;
+
+class EditSiteRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'alpha_num | required | max:255',
+            'favicon' => 'mimes:ico | max:1000',
+            'banner' => 'mimes:jpeg,jpg,bmp,png | max:1000',
+        ];
+    }
+}
