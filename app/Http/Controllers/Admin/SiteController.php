b54828a51296b8d7b2a2b098eb860004899ddd38
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2016-01-24 10:43:29 +0100

    return edit form view for site management in site edit action

--- app/Http/Controllers/Admin/SiteController.php
+++ app/Http/Controllers/Admin/SiteController.php
@@ -5,11 +5,13 @@ namespace Fb\Http\Controllers\Admin;
 use Fb\Http\Requests;
 use Fb\Http\Controllers\Controller;
 use Redirect;
+use Fb\Models\Site;
 
 class SiteController extends Controller
 {
     public function edit()
     {
+        return view('admin.site.edit', ['site' => Site::first()]);
     }
 
     public function update()
