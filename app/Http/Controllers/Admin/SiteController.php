023d36104e93a4ac3627138abacfe451b6b60911
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2016-01-24 09:51:59 +0100

    new site controller

---
+++ app/Http/Controllers/Admin/SiteController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Fb\Http\Controllers\Admin;
+
+use Fb\Http\Requests;
+use Fb\Http\Controllers\Controller;
+use Redirect;
+
+class SiteController extends Controller
+{
+    public function edit()
+    {
+    }
+
+    public function update()
+    {
+    }
+}
