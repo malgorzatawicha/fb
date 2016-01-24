76c606efa9ab5905abbf7e85d4ddaeb8047d7196
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2016-01-24 12:41:52 +0100

    display description of site in meta data tag

--- resources/views/front/layouts/default.blade.php
+++ resources/views/front/layouts/default.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{$site->title}}</title>
-
+    <meta name="description" content="{{$site->description}}" />
     <link type="text/css" rel="stylesheet" href="{{ elixir('css/all.css') }}"/>
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
     @yield('styles')
