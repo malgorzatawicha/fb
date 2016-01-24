79522cf80ab88b5ad8d2cbdee6b6ed91184651f0
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2016-01-24 11:45:03 +0100

    updating site

--- app/Http/Controllers/Admin/SiteController.php
+++ app/Http/Controllers/Admin/SiteController.php
@@ -6,15 +6,33 @@ use Fb\Http\Requests;
 use Fb\Http\Controllers\Controller;
 use Redirect;
 use Fb\Models\Site;
+use Fb\Http\Requests\Site\EditSiteRequest;
+use Fb\Jobs\Site\UpdateSite;
 
 class SiteController extends Controller
 {
     public function edit()
     {
-        return view('admin.site.edit', ['site' => Site::first()]);
+        return view('admin.site.edit', ['site' => $this->getSite()]);
     }
 
-    public function update()
+    public function update(EditSiteRequest $request)
     {
+        $site = $this->getSite();
+        $this->dispatchFromArray(UpdateSite::class, [
+            'site' => $site,
+            'data' => [
+                'title' => $request->get('title'),
+                'description' => $request->get('description'),
+                'favicon' => $request->file('favicon'),
+                'banner' => $request->file('banner')
+            ]
+        ]);
+        return Redirect::route('admin.site.edit');
+    }
+
+    private function getSite()
+    {
+        return Site::firstOrNew([]);
     }
 }
---
+++ app/Jobs/Site/UpdateSite.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Fb\Jobs\Site;
+
+use Fb\Jobs\Job;
+use Fb\Models\Site;
+use Illuminate\Contracts\Bus\SelfHandling;
+use Image;
+
+class UpdateSite extends Job implements SelfHandling
+{
+    protected $site;
+
+    /**
+     * @var array
+     */
+    protected $data = [];
+
+    const DST_FOLDER = '/images/site/';
+    const DST_IMAGE = '';
+
+    protected $absolutePath;
+
+    protected $faviconName = 'favicon.ico';
+    protected $bannerName = 'banner';
+
+    public function __construct(Site $site, array $data)
+    {
+        $this->site = $site;
+        $this->data = $data;
+        $this->absolutePath = public_path();
+    }
+
+    public function handle()
+    {
+        $this->site->title = $this->data['title'];
+        $this->site->description = $this->data['description'];
+        $this->saveFavicon();
+        $this->saveBanner();
+        $this->site->save();
+    }
+
+    private function saveFavicon()
+    {
+        if (!empty($this->data['favicon'])) {
+            $this->data['favicon']->move($this->absolutePath, $this->faviconName);
+        }
+    }
+
+    private function saveBanner()
+    {
+        if (!empty($this->data['banner'])) {
+
+            $imageFile = Image::make($this->data['banner']->getRealPath());
+            $imagePath = $this->saveImageFile($imageFile);
+
+            $this->site->banner_filename = basename($imagePath);
+            $this->site->banner_path = $this->getImagePath();
+        }
+    }
+
+    protected function saveImageFile(\Intervention\Image\Image $image)
+    {
+        $path = $this->getAbsolutePath($this->getImagePath()) . $this->getBannerFileName();
+        $image->save($path);
+        return $path;
+    }
+
+    protected function getAbsolutePath($relativePath)
+    {
+        return $this->absolutePath . $relativePath;
+    }
+
+    protected function getImagePath()
+    {
+        return self::DST_FOLDER . self::DST_IMAGE;
+    }
+
+    protected function getBannerFileName()
+    {
+        $extension =  $this->data['banner']->getClientOriginalExtension();
+        return $this->bannerName . '.' . $extension;
+    }
+}
