2e75478a751c18f92d1140aa1ec1ea79d527166f
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2016-01-24 12:02:09 +0100

    share site data with all views. Display custom site title and favicon in views

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Fb\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Fb\Models\Site;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        view()->share('site', Site::firstOrNew([]));
     }
 
     /**
--- resources/views/front/layouts/default.blade.php
+++ resources/views/front/layouts/default.blade.php
@@ -4,9 +4,10 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>FB Shop</title>
+    <title>{{$site->title}}</title>
 
     <link type="text/css" rel="stylesheet" href="{{ elixir('css/all.css') }}"/>
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
     @yield('styles')
 </head>
 
