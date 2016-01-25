530a70bd75c8ff9eeddc781c3de3c69ad7115d2e
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2016-01-25 18:24:49 +0100

    changes in jobs of creating and updating pages.

--- app/Jobs/Cms/Pages/StorePage.php
+++ app/Jobs/Cms/Pages/StorePage.php
@@ -5,6 +5,7 @@ namespace Fb\Jobs\Cms\Pages;
 use Fb\Jobs\Job;
 use Fb\Models\Cms\Page;
 use Illuminate\Contracts\Bus\SelfHandling;
+use Image;
 
 class StorePage extends Job implements SelfHandling
 {
@@ -13,19 +14,85 @@ class StorePage extends Job implements SelfHandling
      */
     private $data = [];
 
+    const DST_FOLDER = '/images/pages/';
+    const DST_IMAGE = '';
+
+    protected $absolutePath;
+
+    protected $imageFilename;
+
     public function __construct(array $data)
     {
         $this->data = $data;
+        $this->absolutePath = public_path();
     }
 
     public function handle()
     {
         $page = new Page();
-
+        $page->name = !empty($this->data['name'])?$this->data['name']:'';
         $page->title = !empty($this->data['title'])?$this->data['title']:'';
-        $page->body =  !empty($this->data['body'])?$this->data['body']:'';
+        $page->description = !empty($this->data['description'])?$this->data['description']:'';
+        $page->body = !empty($this->data['body'])?$this->data['body']:'';
+        $page->active = $this->data['active'];
+        $this->saveLogo($page);
         $page->save();
 
         return $page;
+    }
+
+    private function saveLogo(Page $page)
+    {
+        if (!empty($this->data['logo'])) {
+            $imageFile = Image::make($this->data['logo']->getRealPath());
+            $imagePath = $this->saveImageFile($imageFile);
+
+            $page->logo_filename = basename($imagePath);
+            $page->logo_path = $this->getImagePath();
+        }
+    }
+
+    protected function saveImageFile(\Intervention\Image\Image $image)
+    {
+        $path = $this->getAbsolutePath($this->getImagePath()) . $this->getImageFileName();
+        $image->save($path);
+        return $path;
+    }
+
+    protected function getImagePath()
+    {
+        return self::DST_FOLDER . self::DST_IMAGE;
+    }
+
+    protected function getAbsolutePath($relativePath)
+    {
+        return $this->absolutePath . $relativePath;
+    }
+
+    protected function getImageFileName()
+    {
+        if (empty($this->imageFilename)) {
+            $extension = $this->data['logo']->getClientOriginalExtension();
+
+            $name = $this->generateFileNameInFolder(
+                $this->getImagePath(),
+                $this->data['logo']->getClientOriginalName(),
+                $extension
+            );
+
+            $this->imageFilename = $name;
+        }
+        return $this->imageFilename;
+    }
+
+    protected function generateFileNameInFolder($path, $basename, $ext)
+    {
+        $name = md5($basename . time()) . '.' . $ext;
+
+        while(\File::exists($path . '/' . $name)) {
+            $name = md5($name . time()) . '.' . $ext;
+        }
+        return $name;
+
     }
 }
--- app/Jobs/Cms/Pages/UpdatePage.php
+++ app/Jobs/Cms/Pages/UpdatePage.php
@@ -5,6 +5,7 @@ namespace Fb\Jobs\Cms\Pages;
 use Fb\Jobs\Job;
 use Fb\Models\Cms\Page;
 use Illuminate\Contracts\Bus\SelfHandling;
+use Image;
 
 class UpdatePage extends Job implements SelfHandling
 {
@@ -18,18 +19,85 @@ class UpdatePage extends Job implements SelfHandling
      */
     private $page;
 
+    const DST_FOLDER = '/images/pages/';
+    const DST_IMAGE = '';
+
+    protected $absolutePath;
+
+    protected $imageFilename;
+
     public function __construct(Page $page, array $data)
     {
         $this->data = $data;
         $this->page = $page;
+        $this->absolutePath = public_path();
     }
 
     public function handle()
     {
+        $this->page->name = !empty($this->data['name'])?$this->data['name']:'';
         $this->page->title = !empty($this->data['title'])?$this->data['title']:'';
-        $this->page->body =  !empty($this->data['body'])?$this->data['body']:'';
+        $this->page->description = !empty($this->data['description'])?$this->data['description']:'';
+        $this->page->body = !empty($this->data['body'])?$this->data['body']:'';
+        $this->page->active = $this->data['active'];
+        $this->saveLogo();
         $this->page->save();
 
         return $this->page;
+    }
+
+    private function saveLogo()
+    {
+        if (!empty($this->data['logo'])) {
+            $imageFile = Image::make($this->data['logo']->getRealPath());
+            $imagePath = $this->saveImageFile($imageFile);
+
+            $this->page->logo_filename = basename($imagePath);
+            $this->page->logo_path = $this->getImagePath();
+        }
+    }
+
+    protected function saveImageFile(\Intervention\Image\Image $image)
+    {
+        $path = $this->getAbsolutePath($this->getImagePath()) . $this->getImageFileName();
+        $image->save($path);
+        return $path;
+    }
+
+    protected function getImagePath()
+    {
+        return self::DST_FOLDER . self::DST_IMAGE;
+    }
+
+    protected function getAbsolutePath($relativePath)
+    {
+        return $this->absolutePath . $relativePath;
+    }
+
+    protected function getImageFileName()
+    {
+        if (empty($this->imageFilename)) {
+            $extension = $this->data['logo']->getClientOriginalExtension();
+
+            $name = $this->generateFileNameInFolder(
+                $this->getImagePath(),
+                $this->data['logo']->getClientOriginalName(),
+                $extension
+            );
+
+            $this->imageFilename = $name;
+        }
+        return $this->imageFilename;
+    }
+
+    protected function generateFileNameInFolder($path, $basename, $ext)
+    {
+        $name = md5($basename . time()) . '.' . $ext;
+
+        while(\File::exists($path . '/' . $name)) {
+            $name = md5($name . time()) . '.' . $ext;
+        }
+        return $name;
+
     }
 }
