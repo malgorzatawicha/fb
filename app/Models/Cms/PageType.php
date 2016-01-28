da6cd6d6012a80c372d204347e1790ebacf90d02
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2016-01-28 19:04:50 +0100

    extend allowable page types.

--- app/Models/Cms/PageType.php
+++ app/Models/Cms/PageType.php
@@ -10,15 +10,39 @@ class PageType
         ['name' => 'custom', 'single' => false]
     ];
 
-    public static function getPossibleTypes()
+    public static function getPossibleTypes(Page $page = null)
     {
         $result = [];
         foreach (static::$types as $type) {
-            if (!$type['single'] || !static::hasPage($type['name'])) {
+            if (static::isAllowable($type, $page)) {
                 $result[] = $type['name'];
             }
         }
         return $result;
+    }
+
+    private static function isAllowable(array $type, Page $page = null)
+    {
+        return (
+            static::pageHasType($type, $page) ||
+            static::isMulti($type) ||
+            static::hasNoPageOf($type)
+        );
+    }
+
+    private static function pageHasType(array $type, Page $page = null)
+    {
+        return (!empty($page) && $page->type == $type['name']);
+    }
+
+    private static function isMulti(array $type)
+    {
+        return empty($type['single']);
+    }
+
+    private static function hasNoPageOf(array $type)
+    {
+        return !static::hasPage($type['name']);
     }
 
     private static function hasPage($type)
