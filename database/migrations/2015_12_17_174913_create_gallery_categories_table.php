a8d7352278a7629433ab5d1932fff442796c1cfd
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2016-02-05 18:11:01 +0100

    add logo and title to gallery category

--- database/migrations/2015_12_17_174913_create_gallery_categories_table.php
+++ database/migrations/2015_12_17_174913_create_gallery_categories_table.php
@@ -14,9 +14,12 @@ class CreateGalleryCategoriesTable extends Migration {
     Schema::create('gallery_categories', function(Blueprint $table) {
       $table->increments('id');
       $table->string('name');
+      $table->string('title');
       $table->string('slug')->unique();
       $table->boolean('active');
       $table->text('description');
+      $table->string('logo_filename')->nullable()->unique();
+      $table->string('logo_path')->nullable();
       $table->integer('parent_id')->nullable()->index();
       $table->integer('lft')->nullable()->index();
       $table->integer('rgt')->nullable()->index();
