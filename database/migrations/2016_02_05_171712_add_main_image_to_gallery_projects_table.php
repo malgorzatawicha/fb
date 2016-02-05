3cf2233f30447e2e5d92094db534f0bf3d90f362
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2016-02-05 18:23:35 +0100

    new table

---
+++ database/migrations/2016_02_05_171712_add_main_image_to_gallery_projects_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddMainImageToGalleryProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('gallery_projects', function($table)
+        {
+            $table->integer('main_image_id')->unsigned();
+            $table->foreign('main_image_id')->references('id')->on('gallery_project_images');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('gallery_projects', function($table)
+        {
+            $table->dropColumn('main_image_id');
+        });
+    }
+}
