f7783231859c970f22d41e3d3d27e215a5ef30c1
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2016-02-05 18:02:59 +0100

    add table connecting page and gallery categories tables

---
+++ database/migrations/2016_02_05_170110_create_table_page_gallery.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTablePageGallery extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('page_friends', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('page_id')->unsigned();
+            $table->foreign('page_id')->references('id')->on('pages');
+            $table->integer('gallery_category_id')->unsigned();
+            $table->foreign('gallery_category_id')->references('id')->on('gallery_categories');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('page_friends');
+    }
+}
