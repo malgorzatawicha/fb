38eb89a445c1e3acd4801a5205206d270691a3b0
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2016-03-20 11:16:49 +0100

    change storing project image job - add only base image and thumb (squared) image

--- app/Jobs/Gallery/ProjectImages/StoreImage.php
+++ app/Jobs/Gallery/ProjectImages/StoreImage.php
@@ -2,11 +2,14 @@
 
 namespace Fb\Jobs\Gallery\ProjectImages;
 
+use Fb\Http\Requests\Galleries\ProjectImages\CreateImageRequest;
 use Fb\Jobs\Job;
+use Fb\Models\File;
 use Fb\Models\Gallery\GalleryProject;
 use Fb\Models\Gallery\GalleryProjectImage;
 use Illuminate\Contracts\Bus\SelfHandling;
 use Image;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class StoreImage extends Job implements SelfHandling
 {
@@ -14,252 +17,102 @@ class StoreImage extends Job implements SelfHandling
      * @var array
      */
     protected $data = [];
+    protected $config = [];
 
+    /**
+     * @var GalleryProject
+     */
     protected $project;
 
+    /**
+     * @var GalleryProjectImage
+     */
     protected $image;
 
-    const DST_FOLDER = '/images/galleries/project_images/';
-    const DST_BASE_IMAGE = '/base/';
-    const DST_BIG_IMAGE = '/big/';
-    const DST_MOBILE_IMAGE = '/mobile/';
-    const DST_MOBILE_THUMB_IMAGE = '/mobile_thumb/';
-    const DST_THUMB_IMAGE = '/thumb/';
+    protected $filename;
 
-    protected $absolutePath;
-
-    protected $thumbnailSize = [
-        'w' => 60,
-        'h' => 60
-    ];
-
-    protected $baseFilename;
-    protected $bigFilename;
-    protected $mobileFilename;
-    protected $mobileThumbFilename;
-    protected $thumbFilename;
-
-    public function __construct(GalleryProject $project, array $data)
+    public function __construct(GalleryProject $project, CreateImageRequest $request)
     {
         $this->project = $project;
-        $this->data = $data;
-        $this->absolutePath = public_path();
+        $this->initialize($request);
     }
 
+    private function initialize(CreateImageRequest $request)
+    {
+        $this->data = [
+            'name' => $request->get('name'),
+            'description' => $request->get('description'),
+            'active' => $request->get('active'),
+            'watermarked' => $request->get('watermarked'),
+            'image' => $request->file('image'),
+            'thumb' => $request->file('thumb')
+        ];
+
+        $this->config = \config('fb.project.image');
+    }
     public function handle()
     {
         $this->image = new GalleryProjectImage([
             'active' => $this->data['active'],
+            'watermarked' => $this->data['watermarked'],
             'name' => $this->data['name'],
             'description' => $this->data['description']
         ]);
         $this->saveBaseImage();
-        $this->saveBigImage();
-        $this->saveMobileImage();
-        $this->saveMobileThumbImage();
         $this->saveThumbImage();
         $this->project->images()->save($this->image);
     }
 
     protected function saveBaseImage()
     {
-        if (!empty($this->data['base_image'])) {
-            $file = Image::make($this->data['base_image']->getRealPath());
-            $path = $this->saveBaseFile($file);
-
-            $this->image->base_filename = basename($path);
-            $this->image->base_path = $this->getBasePath();
+        $image = $this->data['image'];
+        $basePath = $this->config['paths']['base'] . '/' . $this->project->getKey();
+        if (!file_exists($basePath)) {
+            mkdir($basePath);
+            chmod($basePath, 0777);
         }
-    }
-    protected function saveBigImage()
-    {
-        if (!empty($this->data['big_image'])) {
-            $file = Image::make($this->data['big_image']->getRealPath());
-            $path = $this->saveBigFile($file);
-
-            $this->image->big_filename = basename($path);
-            $this->image->big_path = $this->getBigPath();
-        }
-    }
-
-    protected function saveMobileImage()
-    {
-        if (!empty($this->data['mobile_image'])) {
-            $file = Image::make($this->data['mobile_image']->getRealPath());
-            $path = $this->saveMobileFile($file);
-
-            $this->image->mobile_filename = basename($path);
-            $this->image->mobile_path = $this->getMobilePath();
-        }
-    }
-    protected function saveMobileThumbImage()
-    {
-        if (!empty($this->data['mobile_thumb_image'])) {
-            $file = Image::make($this->data['mobile_thumb_image']->getRealPath());
-            $path = $this->saveMobileThumbFile($file);
-
-            $this->image->mobile_thumb_filename = basename($path);
-            $this->image->mobile_thumb_path = $this->getMobileThumbPath();
+        $file = $this->saveImage($image, $basePath);
+        if (!empty($file)) {
+            $this->image->image_id = $file->getKey();
         }
     }
     protected function saveThumbImage()
     {
-        if (!empty($this->data['thumb_image'])) {
-            $file = Image::make($this->data['thumb_image']->getRealPath());
-            $path = $this->saveThumbFile($file);
-
-            $this->image->thumb_filename = basename($path);
-            $this->image->thumb_path = $this->getThumbPath();
+        $image = $this->data['thumb'];
+        $basePath = $this->config['paths']['thumb'] . '/' . $this->project->getKey();
+        if (!file_exists($basePath)) {
+            mkdir($basePath, 0777, true);
+        }
+        $file = $this->saveImage($image, $basePath);
+        if (!empty($file)) {
+            $this->image->thumb_id = $file->getKey();
         }
     }
-    protected function saveBaseFile(\Intervention\Image\Image $image)
-    {
-        $path = $this->getAbsolutePath($this->getBasePath()) . $this->getBaseFileName();
-        $image->save($path);
-        return $path;
-    }
-    protected function saveBigFile(\Intervention\Image\Image $image)
-    {
-        $path = $this->getAbsolutePath($this->getBigPath()) . $this->getBigFileName();
-        $image->save($path);
-        return $path;
-    }
-    protected function saveMobileFile(\Intervention\Image\Image $image)
-    {
-        $path = $this->getAbsolutePath($this->getMobilePath()) . $this->getMobileFileName();
-        $image->save($path);
-        return $path;
-    }
-    protected function saveMobileThumbFile(\Intervention\Image\Image $image)
-    {
-        $path = $this->getAbsolutePath($this->getMobileThumbPath()) . $this->getMobileThumbFileName();
-        $image->save($path);
-        return $path;
-    }
-    protected function saveThumbFile(\Intervention\Image\Image $image)
-    {
-        $path = $this->getAbsolutePath($this->getThumbPath()) . $this->getThumbFileName();
-        $image->save($path);
-        return $path;
-    }
-    protected function getBasePath()
-    {
-        return self::DST_FOLDER . self::DST_BASE_IMAGE;
-    }
 
-    protected function getBigPath()
+    protected function saveImage(UploadedFile $image, $basePath)
     {
-        return self::DST_FOLDER . self::DST_BIG_IMAGE;
-    }
+        if (!empty($image)) {
+            $file = Image::make($image->getRealPath());
+            $fileObject = new File();
+            $fileObject->extension = $image->getClientOriginalExtension();
+            $fileObject->original_filename = $image->getClientOriginalName();
+            $fileObject->path = $basePath;
+            $fileObject->filename = $this->generateFileNameInFolder($fileObject);
 
-    protected function getMobilePath()
-    {
-        return self::DST_FOLDER . self::DST_MOBILE_IMAGE;
-    }
-
-    protected function getMobileThumbPath()
-    {
-        return self::DST_FOLDER . self::DST_MOBILE_THUMB_IMAGE;
-    }
-
-    protected function getThumbPath()
-    {
-        return self::DST_FOLDER . self::DST_THUMB_IMAGE;
-    }
-
-    protected function getAbsolutePath($relativePath)
-    {
-        return $this->absolutePath . $relativePath;
-    }
-
-    protected function generateFileNameInFolder($path, $basename, $ext)
-    {
-            $name = md5($basename . time()) . '.' . $ext;
-
-            while(\File::exists($path . '/' . $name)) {
-                $name = md5($name . time()) . '.' . $ext;
-            }
-            return $name;
-
-    }
-
-    protected function getBaseFileName()
-    {
-        if (empty($this->baseFilename)) {
-            $extension = $this->data['base_image']->getClientOriginalExtension();
-
-            $name = $this->generateFileNameInFolder(
-                $this->getBasePath(),
-                $this->data['base_image']->getClientOriginalName(),
-                $extension
-            );
-
-            $this->baseFilename = $name;
+            $file->save($fileObject->path . '/' . $fileObject->filename);
+            $fileObject->save();
+            return $fileObject;
         }
-        return $this->baseFilename;
+        return null;
     }
-
-    protected function getBigFileName()
+    protected function generateFileNameInFolder(File $file)
     {
-        if (empty($this->bigFilename)) {
-            $extension = $this->data['big_image']->getClientOriginalExtension();
+        $name = md5($file->original_name . time()) . '.' . $file->extension;
 
-            $name = $this->generateFileNameInFolder(
-                $this->getBigPath(),
-                $this->data['big_image']->getClientOriginalName(),
-                $extension
-            );
-
-            $this->bigFilename = $name;
+        while(\File::exists($file->path . '/' . $name)) {
+            $name = md5($name . time()) . '.' . $file->extension;
         }
-        return $this->bigFilename;
-    }
+        return $name;
 
-    protected function getMobileFileName()
-    {
-        if (empty($this->mobileFilename)) {
-            $extension = $this->data['mobile_image']->getClientOriginalExtension();
-
-            $name = $this->generateFileNameInFolder(
-                $this->getMobilePath(),
-                $this->data['mobile_image']->getClientOriginalName(),
-                $extension
-            );
-
-            $this->mobileFilename = $name;
-        }
-        return $this->mobileFilename;
-    }
-
-    protected function getMobileThumbFileName()
-    {
-        if (empty($this->mobileThumbFilename)) {
-            $extension = $this->data['mobile_thumb_image']->getClientOriginalExtension();
-
-            $name = $this->generateFileNameInFolder(
-                $this->getMobileThumbPath(),
-                $this->data['mobile_thumb_image']->getClientOriginalName(),
-                $extension
-            );
-
-            $this->mobileThumbFilename = $name;
-        }
-        return $this->mobileThumbFilename;
-    }
-
-    protected function getThumbFileName()
-    {
-        if (empty($this->thumbFilename)) {
-            $extension = $this->data['thumb_image']->getClientOriginalExtension();
-
-            $name = $this->generateFileNameInFolder(
-                $this->getThumbPath(),
-                $this->data['thumb_image']->getClientOriginalName(),
-                $extension
-            );
-
-            $this->thumbFilename = $name;
-        }
-        return $this->thumbFilename;
     }
 }
