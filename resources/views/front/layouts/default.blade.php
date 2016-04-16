9e6c8cb9b6ba64ee98ee3035b6916f5516172ed0
malgorzata.wicha <malgorzata.wicha@itgrupa.pl>
2016-04-16 13:13:19 +0200

    display correct banner path

--- resources/views/front/layouts/default.blade.php
+++ resources/views/front/layouts/default.blade.php
@@ -15,7 +15,7 @@
 <body>
 <div class="wrapper">
     <div class="container">
-        <img class="img-responsive" src="{{$site->banner_path}}{{$site->banner_filename}}"/>
+        <img class="img-responsive" src="{{route('image', ['fileId' => $site->banner_id, 'width' => 1170, 'height'=>336])}}"/>
         <div class="page-body">
             @yield('content')
         </div>
